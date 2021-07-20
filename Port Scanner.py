d28dc9688161320ae0c6b2568cdbc731afa2c182
kvercher <54039575+kvercher@users.noreply.github.com>
2021-07-20 11:20:09 -0400

    Add files via upload

---
+++ Port Scanner.py
@@ -0,0 +1,40 @@
+import sys
+import socket
+from datetime import datetime
+
+#DEFINE OUR TARGET
+
+if len(sys.argv)==2:
+	target = socket.gethostbyname(sys.argv[1])
+else:
+	print("Invalid amount of arguments.")	
+	print("Syntax: python3 scanner.py <ip>")
+
+#ADD A PRETTY BANNER
+
+print("-"*50)
+print("Scanning target " + target)
+print ("Time started: " + str(datetime.now()))
+print("-"*50)
+
+try:	
+	for port in range(1,500):
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		socket.setdefaulttimeout(1)
+		result = s.connect_ex((target,port))
+		print("Checking port {}".format(port))
+	if result == 0:
+		print("Port {} is open".format(port))
+	s.close()
+
+except KeyboardInterrupt:
+	print("\nExiting program.")
+	sys.exit()
+
+except socket.gaierror:
+	print("Hostname could not be resolved.")
+	sys.exit()
+
+except socket.error:
+	print("Couldn't connect to server.")
+	sys.exit()
